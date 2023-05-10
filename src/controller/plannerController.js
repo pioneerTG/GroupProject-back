f142c3fb50fde809bdec2093fb204ec0380dc072
Tagyeom Park <hhmpsb1412@naver.com>
2023-05-10 19:43:36 +0900

    rev2.1 Modify the JSON post method

--- src/controller/plannerController.js
+++ src/controller/plannerController.js
@@ -1,17 +1,14 @@
-// const { Planner } = require("../models");
 import ExercisePlanner from "../models/ExercisePlan";
 import NutritionPlanner from "../models/NutritionPlan";
 
 export const createExercisePlan = async(req, res, next) => {
   try {
     const { plan } = req.body;
-    const parsedPlan = JSON.parse(plan); // plan을 파싱하여 JSON 객체로 변환
-    console.log('플랜확인:',parsedPlan);
-    console.log('길이 확인:',parsedPlan.length)
-    // const user_id = req.user.id; // 사용자 ID는 인증된 사용자로부터 얻을 수 있어야 함
+    console.log('플랜확인:',plan);
+    console.log('길이 확인:',plan.length)
     // Plan 배열의 각 요소에 대해 DB에 저장
-    for (let i = 0; i < parsedPlan.length; i++) {
-      const { createAt, type, count } = parsedPlan[i];
+    for (let i = 0; i < plan.length; i++) {
+      const { createAt, type, count } = plan[i];
       await ExercisePlanner.create({
         user_id: req.user.id,
         createAt,
@@ -29,13 +26,11 @@ export const createExercisePlan = async(req, res, next) => {
 export const createNutritionPlan = async(req, res, next) => {
   try {
     const { plan } = req.body;
-    const parsedPlan = JSON.parse(plan); // plan을 파싱하여 JSON 객체로 변환
-    console.log('플랜확인:',parsedPlan);
-    console.log('길이 확인:',parsedPlan.length)
-    // const user_id = req.user.id; // 사용자 ID는 인증된 사용자로부터 얻을 수 있어야 함
+    console.log('플랜확인:',plan);
+    console.log('길이 확인:',plan.length)
     // Plan 배열의 각 요소에 대해 DB에 저장
-    for (let i = 0; i < parsedPlan.length; i++) {
-      const { createAt, name, calorie, protein, fat, cho } = parsedPlan[i];
+    for (let i = 0; i < plan.length; i++) {
+      const { createAt, name, calorie, protein, fat, cho } = plan[i];
       await NutritionPlanner.create({
         user_id: req.user.id,
         createAt,
@@ -46,7 +41,7 @@ export const createNutritionPlan = async(req, res, next) => {
         cho,
       });
     }
-    res.status(200).json({ message: "Exercise plan created successfully." });
+    res.status(200).json({ message: "Nutrition plan created successfully." });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error." });
